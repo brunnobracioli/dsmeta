6058380dc080614b6cba1a13bec7dc9a253a2d83
brunnobracioli <brunno_012@hotmail.com>
2022-08-24 16:49:16 -0300

    Inserção de outros componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 import NotificationButton from "./components/notificationButton"
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
+      <Header/>
+      <main>
+        <section id = "sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
