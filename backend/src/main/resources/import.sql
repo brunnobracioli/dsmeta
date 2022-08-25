e4ec0b006ca4c64d2e6400c85eb4bef5b6fc5dc1
brunnobracioli <brunno_012@hotmail.com>
2022-08-24 23:29:48 -0300

    alimentação do banco de dados

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_vendas(nome_vendedor,venda,visita,quantia_vendida,data) VALUES ('Brunno Bracioli',95,80,987098.00,'2022-08-24');
+INSERT INTO tb_vendas(nome_vendedor,venda,visita,quantia_vendida,data) VALUES ('Luna',15,50,238.00,'2022-08-21');
+INSERT INTO tb_vendas(nome_vendedor,venda,visita,quantia_vendida,data) VALUES ('Paco',14,9,38.00,'2022-07-21');
