111d83662644019af4e575283f9c9a574f0e9f97
brunnobracioli <brunno_012@hotmail.com>
2022-08-26 00:27:24 -0300

    Listagem de vendas

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Venda } from "../../models/venda";
+import { BASE_URL } from "../../utils/request";
 
 
 import NotificationButton from '../notificationButton'
@@ -10,41 +12,43 @@ import './styles.css'
 
 function SalesCard() {
 
-    const min = new Date(new Date().setDate(new Date().getDate() -365));
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
     const max = new Date();
 
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    const [venda, setVenda] = useState<Venda[]>([])
+
     useEffect(() => {
-        axios.get("http://localhost:8080/vendas")
-            .then(Response => {
-                console.log(Response.data)
+        axios.get(`${BASE_URL}/vendas`)
+            .then(response => {
+                setVenda(response.data.content)
             })
 
     }, [])
-    
+
 
     return (
-      <><div className="dsmeta-card">
+        <><div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
-                    <DatePicker 
-                    selected={minDate}
-                    onChange={(date: Date) => setMinDate(date)}
-                    className="dsmeta-form-control"
-                    dateFormat="dd/MM/yyyy"
+                    <DatePicker
+                        selected={minDate}
+                        onChange={(date: Date) => setMinDate(date)}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
                     />
                 </div>
                 <div className="dsmeta-form-control-container">
-                <DatePicker 
-                    selected={maxDate}
-                    onChange={(date: Date) => setMaxDate(date)}
-                    className="dsmeta-form-control"
-                    dateFormat="dd/MM/yyyy"
+                    <DatePicker
+                        selected={maxDate}
+                        onChange={(date: Date) => setMaxDate(date)}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
                     />
-                    
+
                 </div>
             </div>
         </div><div>
@@ -61,24 +65,29 @@ function SalesCard() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">24/08/2022</td>
-                            <td>Brunno</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton/>
-                                </div>
-                            </td>
-                        </tr>
+                        {venda.map(venda => {
+                            return (
+                                <tr key ={venda.id}>
+                                    <td className="show992">{venda.id}</td>
+                                    <td className="show576">{new Date(venda.data).toLocaleDateString()}</td>
+                                    <td>{venda.nomeVendedor}</td>
+                                    <td className="show992">{venda.visita}</td>
+                                    <td className="show992">{venda.venda}</td>
+                                    <td>R$ {venda.quantiaVendida.toFixed(2)}</td>
+                                    <td>
+                                        <div className="dsmeta-red-btn-container">
+                                            <NotificationButton />
+                                        </div>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+
                     </tbody>
                 </table>
             </div></>
 
-  )
+    )
 }
 
 export default SalesCard
---
+++ frontend/src/models/venda.ts
@@ -0,0 +1,8 @@
+export type Venda = {
+    data: string;
+    nomeVendedor: string;
+    id: number; 
+    quantiaVendida: number;
+    venda: number;
+    visita: number;
+}
